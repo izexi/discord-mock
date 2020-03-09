d94d43e1636e3bfbf96e28a0d1f4af4740a6cba6
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-03-09 01:17:40 +0000

    feat: setup fastify & basic validation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
-export function sum(a: number, b: number) {
-  return a + b;
+import * as Fastify from 'fastify';
+import Util from './util/Util';
+import { HTTPMethod } from 'fastify';
+
+export const fastify = Fastify();
+const test = process.argv.some(arg => arg.includes('jest'));
+
+fastify.addHook('preValidation', (request, reply, done) => {
+  const authHeader = request.headers.authorization;
+  if (!authHeader || !/Bot \w{24}\.[\w-]{6}\..{27}/.test(authHeader))
+    reply.code(400).send({
+      message: '401: Unauthorized',
+      code: 0
+    });
+  done();
+});
+
+export async function start() {
+  const methodPaths = await Util.flattenPaths('routes', 'methods');
+  await Promise.all(
+    methodPaths.map(methodPath =>
+      fastify.route({
+        method: methodPath.match(/\\methods\\([A-Z]+).[tj]s/)![1] as HTTPMethod,
+        ...require(methodPath).default
+      })
+    )
+  );
+  if (!test) await fastify.listen(3000);
+  return fastify;
 }
+
+if (!test) start();
