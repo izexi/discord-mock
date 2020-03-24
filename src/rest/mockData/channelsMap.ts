3d812b4e225d7f46c41049656a21ad074e2385bc
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-03-24 22:50:32 +0000

    src: create mock data for channels

--- src/rest/mockData/channelsMap.ts
+++ src/rest/mockData/channelsMap.ts
@@ -1,1 +1,24 @@
-export const mockChannel = { id: null };
+import Util from '../../util/Util';
+
+export interface Channel {
+  id: string;
+  name: string;
+  position: number;
+  nsfw: boolean;
+  rate_limit_per_user: number;
+  bitrate?: number;
+  user_limit?: number;
+  // TODO: Type interface for perms
+  permission_overwrites: [];
+  parent_id?: string;
+}
+
+export const mockChannel: Channel = {
+  id: Util.generateID(),
+  name: 'mock',
+  position: 0,
+  nsfw: false,
+  rate_limit_per_user: 0,
+  permission_overwrites: []
+};
+export default Util.mockData(mockChannel);
