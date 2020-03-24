4bca38a8bc222c8628f6e91258e140d8a6b2ed53
Thifyan Ravinehru <zfac171@live.rhul.ac.uk>
2020-03-24 22:49:18 +0000

    feat: add more helper methods and use assigned PORT

--- src/util/Util.ts
+++ src/util/Util.ts
@@ -1,9 +1,31 @@
-import { HTTPMethod } from 'fastify';
+import { HTTPMethod, FastifyRequest, FastifyReply } from 'fastify';
 import { promises as fs } from 'fs';
 import { join } from 'path';
 import fetch from 'node-fetch';
+import { ServerResponse } from 'http';
 
 export default {
+  generateID(map?: Map<string, object>): string {
+    const id = Array.from({ length: 18 }, () =>
+      Math.floor(Math.random() * 10)
+    ).join('');
+    if (!map?.has(id)) return id;
+    return this.generateID(map);
+  },
+  getEntity<T>(
+    type: 'Channel' | 'Message',
+    map: Map<string, T>,
+    request: FastifyRequest,
+    reply: FastifyReply<ServerResponse>
+  ): T | undefined {
+    const entity = map.get(request.params.id);
+    if (!entity)
+      reply.code(404).send({
+        message: `Unknown ${type}`,
+        code: 10013
+      });
+    return entity;
+  },
   mockData<T extends { id: T['id'] }>(data: T | T[]) {
     return new Map(
       Array.isArray(data) ? data.map(this.formatData) : [this.formatData(data)]
@@ -30,18 +52,13 @@ export default {
     url: string,
     body?: { [key: string]: string }
   ) =>
-    fetch(
-      `http://localhost:3000/${url}`,
-      Object.assign(
-        {
-          method,
-          headers: {
-            Authorization:
-              'Bot MzU4NDA0MjA3NDMyMzAyNTkz.XmOtdQ.5LNoSt7UHE0ZAw3cRbDvymBgets',
-            'Content-Type': 'application/json'
-          }
-        },
-        method === 'GET' ? undefined : { body: JSON.stringify(body) }
-      )
-    )
+    fetch(`http://localhost:${process.env.PORT}/${url}`, {
+      method,
+      headers: {
+        Authorization:
+          'Bot MzU4NDA0MjA3NDMyMzAyNTkz.XmOtdQ.5LNoSt7UHE0ZAw3cRbDvymBgets',
+        ...(body ? { 'Content-Type': 'application/json' } : {})
+      },
+      ...(body ? { body: JSON.stringify(body) } : {})
+    })
 };
